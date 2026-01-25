66a0ad8fcd520826329af08ce76162579ab46cb8
SAJDI-HABIB <sajidchughtai779@gmail.com>
2026-01-26 00:17:13 +0500

    Solved a JavaScript Pyramid Pattern Problem!

--- problem-day-4/script.js
+++ problem-day-4/script.js
@@ -8,11 +8,26 @@ for (let i = 1; i <= row; i++) {
   console.log(stars);
 }
 
-let row = 5;
+let row2 = 5;
 for (let i = row; i >= 1; i--) {
   let stars = "";
   for (let j = 1; j <= i; j++) {
     stars += "*";
   }
   console.log(stars);
+}
+
+let row = 5;
+for (let i = 1; i <= 2 * row - 1; i+= 2) {
+    let stars = "";
+    const currRow = (i + 1) / 2;
+    for (let j = 1; j <= row + currRow - 1; j++) {
+        if (j <= row - currRow) {
+            stars += " ";
+        } else{
+            stars += "*"; 
+        }
+    }
+    console.log(stars);
+    
 }
