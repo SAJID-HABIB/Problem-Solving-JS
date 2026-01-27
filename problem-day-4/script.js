d5b376a117559b11bc3fd6ad3f455ac0d52df751
SAJDI-HABIB <sajidchughtai779@gmail.com>
2026-01-28 00:18:03 +0500

    pyramid patterns and right-aligned

--- problem-day-4/script.js
+++ problem-day-4/script.js
@@ -1,33 +1,46 @@
-let row1 = 5;
+// let row1 = 5;
 
-for (let i = 1; i <= row; i++) {
-  let stars = "";
-  for (let j = 1; j <= i; j++) {
-    stars += "*";
-  }
-  console.log(stars);
-}
+// for (let i = 1; i <= row; i++) {
+//   let stars = "";
+//   for (let j = 1; j <= i; j++) {
+//     stars += "*";
+//   }
+//   console.log(stars);
+// }
 
-let row2 = 5;
-for (let i = row; i >= 1; i--) {
-  let stars = "";
-  for (let j = 1; j <= i; j++) {
-    stars += "*";
-  }
-  console.log(stars);
-}
+// let row2 = 5;
+// for (let i = row; i >= 1; i--) {
+//   let stars = "";
+//   for (let j = 1; j <= i; j++) {
+//     stars += "*";
+//   }
+//   console.log(stars);
+// }
 
 let row = 5;
-for (let i = 1; i <= 2 * row - 1; i+= 2) {
-    let stars = "";
-    const currRow = (i + 1) / 2;
-    for (let j = 1; j <= row + currRow - 1; j++) {
-        if (j <= row - currRow) {
-            stars += " ";
-        } else{
-            stars += "*"; 
-        }
+for (let i = 1; i <= 2 * row - 1; i += 2) {
+  let stars = "";
+  let currRow = (i + 1) / 2;
+  for (let j = 1; j <= row + currRow - 1; j++) {
+    if (j <= row - currRow) {
+      stars += " ";
+    } else {
+      stars += "*";
     }
-    console.log(stars);
-    
+  }
+  console.log(stars);
+}
+
+let row1 = 5;
+for (let i = 2 * row1 - 1; i >= 1; i -= 2) {
+  let stars = "";
+  let currRow = (i + 1) / 2;
+  for (let j = 1; j <= row1 + currRow - 1; j++) {
+    if (j <= row1 - currRow) {
+      stars += " ";
+    } else {
+      stars += "*";
+    }
+  }
+  console.log(stars);
 }
