d79e903a0d24ab9e8d85dabdfeb10eff75ed3872
SAJDI-HABIB <sajidchughtai779@gmail.com>
2026-01-29 00:37:34 +0500

    logic and loop boundary problem

--- problem-day-4/script.js
+++ problem-day-4/script.js
@@ -17,30 +17,46 @@
 //   console.log(stars);
 // }
 
-let row = 5;
-for (let i = 1; i <= 2 * row - 1; i += 2) {
-  let stars = "";
-  let currRow = (i + 1) / 2;
-  for (let j = 1; j <= row + currRow - 1; j++) {
-    if (j <= row - currRow) {
-      stars += " ";
-    } else {
-      stars += "*";
-    }
-  }
-  console.log(stars);
-}
 
-let row1 = 5;
-for (let i = 2 * row1 - 1; i >= 1; i -= 2) {
-  let stars = "";
-  let currRow = (i + 1) / 2;
-  for (let j = 1; j <= row1 + currRow - 1; j++) {
-    if (j <= row1 - currRow) {
-      stars += " ";
-    } else {
-      stars += "*";
-    }
+// let row = 5;
+// for (let i = 1; i <= 2 * row - 1; i += 2) {
+//   let stars = "";
+//   let currRow = (i + 1) / 2;
+//   for (let j = 1; j <= row + currRow - 1; j++) {
+//     if (j <= row - currRow) {
+//       stars += " ";
+//     } else {
+//       stars += "*";
+//     }
+//   }
+//   console.log(stars);
+// }
+
+// let row1 = 5;
+// for (let i = 2 * row1 - 1; i >= 1; i -= 2) {
+//   let stars = "";
+//   let currRow = (i + 1) / 2;
+//   for (let j = 1; j <= row1 + currRow - 1; j++) {
+//     if (j <= row1 - currRow) {
+//       stars += " ";
+//     } else {
+//       stars += "*";
+//     }
+//   }
+//   console.log(stars);
+// }
+
+
+let row = 5;
+for (let i = 1; i <= row; i++) {
+  // let stars = "";
+  for (let j = 0; j <= row; j++) {
+   if (row === 1 || row === 5 || j === 1 || j === row) {
+    console.log("*");
+   } else {
+    console.log("\n");
+    
+   }
+    
   }
-  console.log(stars);
 }
