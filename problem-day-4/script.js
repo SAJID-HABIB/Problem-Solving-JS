139f18976bebdc0971bd6d32ee92afd25732e86e
SAJDI-HABIB <sajidchughtai779@gmail.com>
2026-02-01 22:35:45 +0500

    triangle of alternating 1s and 0s

--- problem-day-4/script.js
+++ problem-day-4/script.js
@@ -1,8 +1,8 @@
-// let row1 = 5;
+// let r1 = 5;
 
 // const { line, li } = require("framer-motion/client");
 
-// for (let i = 1; i <= row; i++) {
+// for (let i = 1; i <= r; i++) {
 //   let stars = "";
 //   for (let j = 1; j <= i; j++) {
 //     stars += "*";
@@ -10,8 +10,8 @@
 //   console.log(stars);
 // }
 
-// let row2 = 5;
-// for (let i = row; i >= 1; i--) {
+// let r2 = 5;
+// for (let i = r; i >= 1; i--) {
 //   let stars = "";
 //   for (let j = 1; j <= i; j++) {
 //     stars += "*";
@@ -19,12 +19,12 @@
 //   console.log(stars);
 // }
 
-// let row = 5;
-// for (let i = 1; i <= 2 * row - 1; i += 2) {
+// let r = 5;
+// for (let i = 1; i <= 2 * r - 1; i += 2) {
 //   let stars = "";
-//   let currRow = (i + 1) / 2;
-//   for (let j = 1; j <= row + currRow - 1; j++) {
-//     if (j <= row - currRow) {
+//   let currr = (i + 1) / 2;
+//   for (let j = 1; j <= r + currr - 1; j++) {
+//     if (j <= r - currr) {
 //       stars += " ";
 //     } else {
 //       stars += "*";
@@ -33,22 +33,22 @@
 //   console.log(stars);
 // }
 
-// let row1 = 5;
-// for (let i = 2 * row1 - 1; i >= 1; i -= 2) {
+// let r1 = 5;
+// for (let i = 2 * r1 - 1; i >= 1; i -= 2) {
 //   let stars = "";
-//   let currRow = (i + 1) / 2;
-//   for (let j = 1; j <= row1 + currRow - 1; j++) {
-//     if (j <= row1 - currRow) {
+//   let currr = (i + 1) / 2;
+//   for (let j = 1; j <= r1 + currr - 1; j++) {
+//     if (j <= r1 - currr) {
 //       stars += " ";
 //     } else {
 //       stars += "*";
 //     }
-// let row1 = 5;
-// for (let i = 2 * row1 - 1; i >= 1; i -= 2) {
+// let r1 = 5;
+// for (let i = 2 * r1 - 1; i >= 1; i -= 2) {
 //   let stars = "";
-//   let currRow = (i + 1) / 2;
-//   for (let j = 1; j <= row1 + currRow - 1; j++) {
-//     if (j <= row1 - currRow) {
+//   let currr = (i + 1) / 2;
+//   for (let j = 1; j <= r1 + currr - 1; j++) {
+//     if (j <= r1 - currr) {
 //       stars += " ";
 //     } else {
 //       stars += "*";
@@ -61,11 +61,11 @@
 //   console.log(stars);
 // }
 
-// let row = 5;
-// for (let i = 1; i <= row; i++) {
+// let r = 5;
+// for (let i = 1; i <= r; i++) {
 //   // let stars = "";
-//   for (let j = 0; j <= row; j++) {
-//    if (row === 1 || row === 5 || j === 1 || j === row) {
+//   for (let j = 0; j <= r; j++) {
+//    if (r === 1 || r === 5 || j === 1 || j === r) {
 //     console.log("*");
 //    } else {
 //     console.log("\n");
@@ -75,13 +75,13 @@
 //   }
 // }
 
-// let row = 5;
-// for (let i = 1; i <= row; i++) {
+// let r = 5;
+// for (let i = 1; i <= r; i++) {
 //   let line = "";
-//   for (let j = 1; j <= row; j++) {
-//     if (i === 1 || i === row) {
+//   for (let j = 1; j <= r; j++) {
+//     if (i === 1 || i === r) {
 //       line += "*";
-//     } else if (j === 1 || j === row) {
+//     } else if (j === 1 || j === r) {
 //       line += "*";
 //     } else{
 //       line += " ";
@@ -92,47 +92,47 @@
 
 // }
 
-// let row = 5;
-// for (let i = 1; i <= row; row - i) {
+// let r = 5;
+// for (let i = 1; i <= r; r - i) {
 //   line = "";
-//   for (let j = 0; j <= row; 2 * (i - 1) - 1) {
+//   for (let j = 0; j <= r; 2 * (i - 1) - 1) {
 //     line += " ";
-//     if (j === row) {
+//     if (j === r) {
 //       line = "*";
 //     }
 //   }
 //   console.log(line);
 // }
 
-let row1 = 5;
-for (let i = 1; i <= 2 * row - 1; i += 2) {
-  let stars = "";
-  let currRow = (i + 1) / 2;
-  for (let j = 1; j <= row + currRow - 1; j++) {
-    if (
-      j <= row - currRow ||
-      (j > row - currRow + 1 && j < row + currRow - 1 && i !== 2 * row - 1)
-    ) {
-      stars += " ";
-    } else {
-      stars += "*";
-    }
-  }
-  console.log(stars);
-}
+// let r1 = 5;
+// for (let i = 1; i <= 2 * r - 1; i += 2) {
+//   let stars = "";
+//   let currr = (i + 1) / 2;
+//   for (let j = 1; j <= r + currr - 1; j++) {
+//     if (
+//       j <= r - currr ||
+//       (j > r - currr + 1 && j < r + currr - 1 && i !== 2 * r - 1)
+//     ) {
+//       stars += " ";
+//     } else {
+//       stars += "*";
+//     }
+//   }
+//   console.log(stars);
+// }
 
 let row = 5;
 
-for (let i = row; i >= 1; i--) {
+for (let i = r; i >= 1; i--) {
   let stars = "";
-  for (let j = 1; j <= row + i - 1; j++) {
-    if (i === row) {
+  for (let j = 1; j <= r + i - 1; j++) {
+    if (i === r) {
       stars += "*";
-    } else if (j <= row - i) {
+    } else if (j <= r - i) {
       stars += " ";
-    } else if (j === row - i + 1) {
+    } else if (j === r - i + 1) {
       stars += "*";
-    } else if (j === row + i - 1) {
+    } else if (j === r + i - 1) {
       stars += "*";
     } else {
       stars += " ";
@@ -140,4 +140,14 @@ for (let i = row; i >= 1; i--) {
   }
 
   console.log(stars);
+}
+
+let r = 5;
+
+for (let i = 1; i <= r; i++) {
+  let digit = "";
+  for (let j = 1; j <= i; j++) {
+    digit += (i + j + 2) % 2;
+  }
+  console.log(digit);
 }
